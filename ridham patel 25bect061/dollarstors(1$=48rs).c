a8f86770c97aa22e9f47f733c2afc1e3d1cf0702
ridham005 <ridham0507@gmail.com>
2025-08-09 15:26:54 +0530

    Add files via upload

---
+++ ridham patel 25bect061/dollarstors(1$=48rs).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ int main()
+ {
+     float dollars,rupees;
+     printf("Enter dollars:");
+     scanf("%f",&dollars);
+     rupees = dollars * 45;
+     printf("%f * 45 = %f\n",dollars,rupees);
+     return 0;
+ }
+
